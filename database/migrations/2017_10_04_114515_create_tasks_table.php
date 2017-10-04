764b9c37bfdb31ce704cf30a9af5b3d31efaa46d
Tomoki Iwaoka <tomokiiwaoka@gmail.com>
2017-10-04 02:57:14 +0000

    migrate tasks table

---
+++ database/migrations/2017_10_04_114515_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content'); // add for managing tasks 2017/10/4
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
